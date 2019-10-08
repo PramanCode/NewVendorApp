867b7eea08e1fe2b0bad047e7dd32ab383a0270f
Praman Gupta <praman@localcircles.com>
2019-10-08 17:02:52 +0530

    Basic Functional Structure & UI Prototype(Correct Push)
    It contains all the basic functions required & UI prototype that need to be advanced
     master

---
+++ ItemsDetailsPage.js
@@ -0,0 +1,451 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  View,
+  TouchableWithoutFeedback,
+  Modal,
+  Switch
+} from "react-native";
+import { StackNavigator } from "react-navigation";
+import firebase from "firebase";
+import {HeaderButtons, HeaderButton,Item, HiddenItem} from "react-navigation-header-buttons";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+export default class ItemsDetailsPage extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerRight: (
+        <View>
+          <HeaderButtons
+            HeaderButtonComponent= {()=> {<HeaderButton IconComponent={Ionicons} iconSize={23} color="blue" />}}
+            OverflowIcon={<MaterialIcons name="more-vert" size={23} />}
+          >
+            <Item
+              title="Add Food Item"
+              onPress={() => navigation.state.params.itemModalHandler(true)}
+              show="never"
+            />
+            <Item
+              title="Sign Out"
+              onPress={() => navigation.state.params.signOutHandler()}
+              show="never"
+            />
+          </HeaderButtons>
+        </View>
+      )
+    };
+  };
+
+  constructor(props) {
+    super(props);
+    this.itemKey;
+    this.itemIndex = null;
+    this.restaurantKey = this.props.navigation.getParam("restaurantKey");
+    this.categoryKey = this.props.navigation.getParam("categoryKey");
+    this.categoryName = this.props.navigation.getParam("categoryName");
+    console.log(
+      "constructor item ItemsDetailsPage" +
+        this.restaurantKey +
+        this.categoryKey
+    );
+    this.state = {
+      itemModalVisible: false,
+      editItemModalVisible: false,
+      itemArr: [],
+      itemName: "",
+      itemPrice: ""
+    };
+    this.fetchItemArr = this.fetchItemArr.bind(this);
+    this.renderItemModal = this.renderItemModal.bind(this);
+    this.itemModalHandler = this.itemModalHandler.bind(this);
+    this.addFoodItem = this.addFoodItem.bind(this);
+    this.signOutHandler = this.signOutHandler.bind(this);
+    this.updateFoodItem = this.updateFoodItem.bind(this);
+    this.toggleItem = this.toggleItem.bind(this);
+    this.deleteFoodItem = this.deleteFoodItem.bind(this);
+    this.fetchItemArr().then(itemArrState => {
+      this.setState({ itemArr: itemArrState });
+    });
+    this.props.navigation.setParams({
+      itemModalHandler: this.itemModalHandler.bind(this),
+      signOutHandler: this.signOutHandler.bind(this)
+    });
+  }
+
+  async fetchItemArr() {
+    console.log("fetch Item Arr" + this.restaurantKey);
+    return firebase
+      .database()
+      .ref(
+        "/Restaurants/" +
+          this.restaurantKey +
+          "/Categories/" +
+          this.categoryKey +
+          "/FoodItems"
+      )
+      .once("value")
+      .then(function(snapshot) {
+        var itemArr = [];
+        if (snapshot.val() != null) {
+          Object.keys(snapshot.val()).forEach(function(key) {
+            let element = snapshot.val()[key];
+            element.itemKey = key;
+            itemArr.push(element);
+          });
+        }
+        return itemArr;
+      })
+      .catch(error => console.log(error));
+  }
+
+  renderItemModal(value) {
+    let SuitableButton, DeleteButton;
+    console.log("item index: " + this.itemIndex);
+    let currentItemName = "";
+    let curerntItemPrice = "";
+    console.log("renderItemModal itemIndex : " + this.itemIndex);
+    console.log(
+      "restaurantKey renderItemModal ItemsDetailsPage" + this.restaurantKey
+    );
+    if (value) {
+      SuitableButton = (
+        <Button title={"Add Item"} onPress={e => this.addFoodItem(e)} />
+      );
+    } else {
+      SuitableButton = (
+        <Button title={"Update Item"} onPress={e => this.updateFoodItem(e)} />
+      );
+      DeleteButton = (
+        <Button title={"Delete Item"} onPress={e => this.deleteFoodItem(e)} />
+      );
+      if (this.itemIndex != null && !value && this.state.editItemModalVisible) {
+        console.log(this.state.itemArr[this.itemIndex]);
+        currentItemName = this.state.itemArr[this.itemIndex].itemName;
+        curerntItemPrice = this.state.itemArr[this.itemIndex].itemPrice;
+      }
+    }
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "column",
+            width:200,
+            height:300,
+            paddingBottom:70,
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 4,
+            borderColor: "#888",
+            borderRadius: 10,
+            backgroundColor: "#fff"
+          }}
+        >
+          <TextInput
+            style={{ backgroundColor: "transparent",marginTop:70,width:150,paddingBottom:10,marginBottom:10,paddingLeft:5}}
+            defaultValue={currentItemName}
+            placeholder ="Enter Item Name"
+            onChangeText={itemName => {
+              this.setState({ itemName: itemName });
+            }}
+          />
+          <TextInput
+            style={{ backgroundColor: "transparent",width:150,paddingBottom:10,marginBottom:10,paddingLeft:5}}
+            defaultValue={curerntItemPrice}
+            placeholder = "Enter Price"
+            onChangeText={itemPrice => {
+              this.setState({ itemPrice: itemPrice });
+            }}
+          />
+          <View style = {{marginBottom:10}}>
+          {SuitableButton}
+          </View>
+          <View style = {{marginBottom:10}}>
+          {DeleteButton}
+          </View>
+          <Button
+            title="Cancel"
+            onPress={e => {
+              e.preventDefault();
+              this.setState({
+                editItemModalVisible: false,
+                itemModalVisible:false
+              });
+            }}
+          />
+          </View>
+      </View>
+    );
+  }
+
+  addFoodItem(e) {
+    e.preventDefault(e);
+
+    //create a key in the database for the new item under
+    /*restaurantKey = this.props.navigation.getParam("restaurantKey");
+    categoryKey = this.props.navigation.getParam("categoryKey");*/
+    console.log("restaurantKey food item" + this.restaurantKey);
+    this.itemKey = firebase
+      .database()
+      .ref(
+        "/Restaurants/" +
+          this.restaurantKey +
+          "/Categories/" +
+          this.categoryKey +
+          "/FoodItems"
+      )
+      .push().key;
+    console.log(" food item key generrated" + this.itemKey.toString());
+
+    //restaurant key under which category is to be created
+
+    //add item details that needed to be added in firebase
+    var updates = {};
+    updates[
+      "/Restaurants/" +
+        this.restaurantKey +
+        "/Categories/" +
+        this.categoryKey +
+        "/FoodItems/" +
+        this.itemKey +
+        "/itemName"
+    ] = this.state.itemName;
+    updates[
+      "/Restaurants/" +
+        this.restaurantKey +
+        "/Categories/" +
+        this.categoryKey +
+        "/FoodItems/" +
+        this.itemKey +
+        "/itemPrice"
+    ] = this.state.itemPrice;
+    updates[
+      "/Restaurants/" +
+        this.restaurantKey +
+        "/Categories/" +
+        this.categoryKey +
+        "/FoodItems/" +
+        this.itemKey +
+        "/itemAvailable"
+    ] = true;
+
+    //send updates to firebase
+    firebase
+      .database()
+      .ref()
+      .update(updates);
+
+    //add item to local array that will be displayed
+    this.state.itemArr.push({
+      itemKey: this.itemKey,
+      itemName: this.state.itemName,
+      itemPrice: "0",
+      itemAvailable: true
+    });
+
+    //re-render the component with newly added item
+    this.setState({ itemModalVisible: false, itemArr: this.state.itemArr });
+  }
+
+  updateFoodItem(e) {
+    e.preventDefault();
+    var updates = {};
+    updates[
+      "/Restaurants/" +
+        this.restaurantKey +
+        "/Categories/" +
+        this.categoryKey +
+        "/FoodItems/" +
+        this.itemKey +
+        "/itemName"
+    ] = this.state.itemName;
+    updates[
+      "/Restaurants/" +
+        this.restaurantKey +
+        "/Categories/" +
+        this.categoryKey +
+        "/FoodItems/" +
+        this.itemKey +
+        "/itemPrice"
+    ] = this.state.itemPrice;
+    let itemArrState = this.state.itemArr;
+    console.log("item index real: " + this.itemIndex);
+    itemArrState[this.itemIndex].itemName = this.state.itemName;
+    itemArrState[this.itemIndex].itemPrice = this.state.itemPrice;
+    //send updates to firebase
+    firebase
+      .database()
+      .ref()
+      .update(updates)
+      .then(() => {
+        this.setState({ editItemModalVisible: false, itemArr: itemArrState });
+      })
+      .catch(error => {
+        console.log(error);
+        alert("it cant be done");
+        this.setState({ editItemModalVisible: false });
+      });
+  }
+
+  deleteFoodItem(e) {
+    console.log(
+      "came to delete restaurantKey " +
+        this.restaurantKey +
+        " categoryKey " +
+        this.categoryKey +
+        " itemKey " +
+        this.itemKey
+    );
+
+    firebase
+      .database()
+      .ref(
+        "/Restaurants/" +
+          this.restaurantKey +
+          "/Categories/" +
+          this.categoryKey +
+          "/FoodItems/" +
+          this.itemKey
+      )
+      .remove()
+      .then(() => {
+        this.fetchItemArr()
+          .then(updatedItemArr => {
+            this.setState({
+              editItemModalVisible: !this.state.editItemModalVisible,
+              itemArr: updatedItemArr
+            });
+          })
+          .catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
+  }
+
+  itemModalHandler(value) {
+    //e.preventDefault();
+    if (value) {
+      console.log("called");
+      this.setState({ itemModalVisible: true});
+    } else {
+      console.log("called");
+      this.setState({ editItemModalVisible: !this.state.editItemModalVisible });
+    }
+  }
+
+  toggleItem(i) {
+    //a.categoryAvailable=!a.categoryAvailable;
+    let itemArrState = this.state.itemArr;
+    //console.log(this.state.itemArr[i].toString());
+    itemArrState[i].itemAvailable = !this.state.itemArr[i].itemAvailable;
+    firebase
+      .database()
+      .ref(
+        "/Restaurants/" +
+          this.restaurantKey +
+          "/Categories/" +
+          this.categoryKey +
+          "/FoodItems/" +
+          itemArrState[i].itemKey
+      )
+      .update({ itemAvailable: itemArrState[i].itemAvailable })
+      .then(() => {
+        this.setState({ itemArr: itemArrState });
+      })
+      .catch(error => console.log(error));
+    console.log(
+      "key and category while rendering " +
+        this.state.itemArr[i].itemName +
+        " " +
+        itemArrState[i].itemAvailable
+    );
+  }
+
+  signOutHandler() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.props.navigation.navigate("Login");
+      });
+  }
+
+  render() {
+    console.log("inside render restaurantKey " + this.restaurantKey);
+    var itemListArr = this.state.itemArr.map((a, i) => {
+      console.log(
+        "key and category while rendering+" + a.itemKey + " " + a.itemName
+      );
+      return (
+        <View
+          key={a.itemKey}
+          style={{ borderBottomWidth: 2, borderBottomColor: "#ededed" }}
+        >
+          <View>
+            <TouchableWithoutFeedback
+              onLongPress={e => {
+                this.itemIndex = i;
+                this.itemKey = a.itemKey;
+                this.itemModalHandler(false);
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "#c9c9c5",
+                  height: 40,
+                  flexDirection: "row"
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "left",
+                    justifyContent: "center",
+                    flex: 1
+                  }}
+                >
+                  {a.itemName}
+                </Text>
+                <Switch
+                  onValueChange={() => this.toggleItem(i)}
+                  value={this.state.itemArr[i].itemAvailable}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </View>
+      );
+    });
+    return (
+      <View>
+        {itemListArr}
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.itemModalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+          }}
+        >
+          {this.renderItemModal(true)}
+        </Modal>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.editItemModalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+          }}
+        >
+          {this.renderItemModal(false)}
+        </Modal>
+      </View>
+    );
+  }
+}
---
+++ Old App Files/RestaurantDetails.js
@@ -0,0 +1,324 @@
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Modal
+} from "react-native";
+//import makeConnection from './Connection';
+import { StackNavigator } from "react-navigation";
+import Toast from "react-native-simple-toast";
+import * as firebase from "firebase";
+import Alert from "Alert";
+import { Constants, Location, Permissions } from "expo";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+const homePlace = {
+  description: "Home",
+  geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }
+};
+const workPlace = {
+  description: "Work",
+  geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }
+};
+
+export default class RestaurantDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurantName: "",
+      restaurantAddress: "",
+      restaurantEmail: "",
+      restaurantContactNo: "",
+      searchModalVisible: false
+    };
+
+    this.addressLatitude = 0;
+    this.addressLongitude = 0;
+    this.checkRestaurantDetails = this.checkRestaurantDetails.bind(this);
+    this.sendRestaurantDetails = this.sendRestaurantDetails.bind(this);
+    this.renderSearchModal = this.renderSearchModal.bind(this);
+    this.sendLocationAlert = this.sendLocationAlert.bind(this);
+    this.getLocationAsync = this.getLocationAsync.bind(this);
+
+    console.log("RestaurantDetails console"+this.props.navigation.getParam("userKey"));
+
+    if (this.props.navigation.getParam("restaurantKey")) {
+      console.log("checking" + this.props.navigation.getParam("restaurantKey"));
+
+      //var yes = async ()=>{return firebase.database().ref("/Restaurants/"+this.props.navigation.getParam("restaurantKey")).once("value")}
+
+      this.fetchRestaurantDetails()
+        .then(snapshot => {
+          console.log("here 2");
+          this.setState({
+            restaurantName: snapshot.val()["Restaurant Name"],
+            restaurantAddress: snapshot.val()["Address"],
+            restaurantEmail: snapshot.val()["Email"],
+            restaurantContactNo: snapshot.val()["Contact No"],
+            searchModalVisible: false
+          });
+
+          console.log("checking" + snapshot.val()["Email"]);
+        })
+        .catch(error => console.log("checking" + error));
+    } else {
+      console.log("here");
+    }
+  }
+
+  async fetchRestaurantDetails() {
+    console.log("fetch Item Arr");
+    return firebase
+      .database()
+      .ref("/Restaurants/" + this.props.navigation.getParam("restaurantKey"))
+      .once("value");
+  }
+
+  checkRestaurantDetails() {
+    if (this.state.restaurantName == "") {
+      Toast.show("Restaurant name cannot be empty");
+      return;
+    }
+    if (this.state.restaurantEmail.length == "") {
+      Toast.show("Email cannot be empty");
+      return;
+    }
+    var emailFormat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (emailFormat.test(this.state.restaurantEmail) == false) {
+      Toast.show("Email format should be correct");
+      return;
+    }
+    if (this.state.restaurantAddress == "") {
+      Toast.show("Adress cannot be empty");
+      return;
+    }
+    if (this.state.restaurantContactNo.length < 9) {
+      Toast.show("Contact No. is not valid");
+      return;
+    }
+    this.sendRestaurantDetails();
+  }
+  renderSearchModal() {
+    const GooglePlacesInput = () => {
+      return (
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          minLength={2} // minimum length of text to search
+          autoFocus={true}
+          // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+          listViewDisplayed="auto" // true/false/undefined
+          fetchDetails={true}
+          renderDescription={row => row.description} // custom description render
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            this.setState({
+              restaurantAddress: data.description,
+              searchModalVisible: false
+            });
+            console.log(JSON.stringify(details.geometry.location));
+            this.addressLatitude = details.geometry.location.lat;
+            this.addressLongitude = details.geometry.location.lng;
+            //console.log(details);
+          }}
+          getDefaultValue={() => {
+            return ""; // text input default value
+          }}
+          query={{
+            // available options: https://developers.google.com/places/web-service/autocomplete
+            //AIzaSyC2l9cnyic79pMnidRRDk7mOQjZhRnz0jE
+            //AIzaSyDy1CBCpnXU5vqPq6U524QDMULrSrYqxzI
+            key: "AIzaSyC2l9cnyic79pMnidRRDk7mOQjZhRnz0jE",
+            language: "en", // language of the results
+            types: "address" // default: 'geocode'
+          }}
+          styles={{
+            description: {
+              fontWeight: "bold"
+            },
+            predefinedPlacesDescription: {
+              color: "#1faadb"
+            }
+          }}
+        />
+      );
+    };
+    return <GooglePlacesInput />;
+  }
+
+  getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true
+    });
+    this.addressLatitude = location.coords.latitude;
+    this.addressLongitude = location.coords.longitude;
+    console.log(location);
+    console.log(this.addressLatitude + " " + this.addressLongitude);
+    let addressArray = await Location.reverseGeocodeAsync({
+      latitude: this.addressLatitude,
+      longitude: this.addressLongitude
+    });
+    let address =
+      addressArray[0].name +
+      ", " +
+      addressArray[0].street +
+      ", " +
+      addressArray[0].city +
+      ", " +
+      addressArray[0].postalCode +
+      ", " +
+      addressArray[0].region +
+      ", " +
+      addressArray[0].country;
+    console.log(JSON.stringify(addressArray[0]));
+    console.log(address);
+    this.setState({ restaurantAddress: address });
+  };
+  sendRestaurantDetails() {
+    var fdatabase = firebase.database();
+    var restaurantKey = fdatabase.ref("Restaurants").push().key;
+    fdatabase.ref("/Restaurants/" + restaurantKey).update({
+      "Restaurant Name": this.state.restaurantName,
+      Address: this.state.restaurantAddress,
+      Email: this.state.restaurantEmail,
+      "Contact No": this.state.restaurantContactNo,
+      Latitude: this.addressLatitude,
+      Longitude: this.addressLongitude,
+      Available: true
+    });
+    console.log("userKey " + this.props.navigation.getParam("userKey"));
+    fdatabase.ref().update({
+      ["users/" +
+      this.props.navigation.getParam("userKey") +
+      "/restaurantKey"]: restaurantKey
+    });
+    alert("done");
+    console.log("restaurantKey on Restaurants page: " + restaurantKey);
+    this.props.navigation.navigate("Items", { restaurantKey: restaurantKey });
+  }
+
+  updateRestaurantDetails() {
+    console.log(Location.geocodeAsync(this.state.restaurantAddress));
+
+    var fdatabase = firebase.database();
+    var updates = {};
+    updates["/Restaurants/" + this.props.navigation.getParam("restaurantKey")+"/Restaurant Name"] = this.state.restaurantName;
+    updates["/Restaurants/" + this.props.navigation.getParam("restaurantKey")+"/Address"] = this.state.restaurantAddress;
+    updates["/Restaurants/" + this.props.navigation.getParam("restaurantKey")+"/Email"] = this.state.restaurantEmail;
+    updates["/Restaurants/" + this.props.navigation.getParam("restaurantKey")+"/Contact No"] = this.state.restaurantContactNo;
+    updates["/Restaurants/" + this.props.navigation.getParam("restaurantKey")+"/Latitude"] = this.addressLatitude;
+    updates["/Restaurants/" + this.props.navigation.getParam("restaurantKey")+"/Longitude"] = this.addressLongitude;
+    fdatabase.ref().update(updates);
+    
+    this.props.navigation.navigate("Items", {
+      restaurantKey: this.props.navigation.getParam("restaurantKey")
+    });
+  }
+
+  sendLocationAlert() {
+    {
+      Alert.alert(
+        "Enter Location",
+        "Choose an option",
+        [
+          {
+            text: "Turn On Location",
+            onPress: () => {
+              this.getLocationAsync();
+            }
+          },
+          {
+            text: "Enter Manually",
+            onPress: () => this.setState({ searchModalVisible: true })
+          }
+        ],
+        { cancelable: false }
+      );
+    }
+  }
+  render() {
+    let SuitableButton;
+    if (this.props.navigation.getParam("restaurantKey")) {
+      SuitableButton = (
+        <Button
+          title={"Update"}
+          onPress={() => this.updateRestaurantDetails()}
+          color="#841584"
+        />
+      );
+    } else {
+      SuitableButton = (
+        <Button
+          title={"Next"}
+          onPress={() => this.sendRestaurantDetails()}
+          color="#841584"
+        />
+      );
+    }
+    console.log(this.state.restaurantName + " " + this.state.restaurantAddress);
+    return (
+      <View style={{ flex: 1, margin: 30, justifyContent: "center",backgroundColor: "rgb(242, 244, 247)"}}>
+        <View style ={{ backgroundColor: "#fff" }}>
+          <TextInput
+            style={{ paddingBottom: 10, paddingLeft: 10 }}
+            defaultValue={this.state.restaurantName}
+            placeholder="Restaurant Name"
+            onChangeText={RestaurantName =>
+              this.setState({ restaurantName: RestaurantName })
+            }
+          />
+
+          <TextInput
+            style={{ paddingBottom: 10, paddingLeft: 10 }}
+            defaultValue={this.state.restaurantEmail}
+            placeholder="Email"
+            onChangeText={RestaurantEmail =>
+              this.setState({ restaurantEmail: RestaurantEmail })
+            }
+          />
+          <TextInput
+            style={{ paddingBottom: 10, paddingLeft: 10 }}
+            placeholder="Address"
+            defaultValue={this.state.restaurantAddress}
+            onChangeText={RestaurantAdresss =>
+              this.setState({ restaurantAddress: RestaurantAdresss })
+            }
+            onFocus={() => this.sendLocationAlert()}
+          />
+
+          <TextInput
+            style={{ paddingBottom: 10, paddingLeft: 10, marginBottom: 60 }}
+            keyboardType='numeric'
+            defaultValue={this.state.restaurantContactNo}
+            placeholder="Contact No"
+            onChangeText={ContactNo =>
+              this.setState({ restaurantContactNo: ContactNo })
+            }
+          />
+          {SuitableButton}
+        </View>
+        <Modal
+          animationType="fade"
+          visible={this.state.searchModalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+            this.setState({ searchModalVisible: false });
+          }}
+        >
+          {this.renderSearchModal()}
+        </Modal>
+      </View>
+    );
+  }
+}
